e91c78beab78d2eb890053673e1e04a5e8ebf356
Eric D. Helms <ericdhelms@gmail.com>
2013-07-29 16:31:06 -0400

    Engine: Cleaning up Gemfile and gemspec.

--- lib/katello.rb
+++ lib/katello.rb
@@ -2,8 +2,9 @@ require "katello/engine"
 
 require "katello/load_configuration"
 require "katello/logging"
-#require "apipie-rails"
-#require "rabl"
+require "apipie-rails"
+
+require "rabl"
 require "tire"
 #require "oauth"
 #require "openid"
@@ -11,11 +12,23 @@ require "tire"
 #require "gettext_i18n_rails"
 require "ruport/acts_as_reportable"
 #require "runcible"
-#require "hooks"
 #require "haml-rails"
 #require "ui_alchemy-rails"
 #require "logger"
 require "foreigner"
+require "hooks"
+require "logger"
+require "foreigner"
+require "dynflow"
+require "rest_client"
+require "i18n_data"
+require "justified/standard_error"
+
+require "haml-rails"
+require "compass-rails"
+require "ui_alchemy-rails"
+
+require "headpin"
 
 module Katello
 end
